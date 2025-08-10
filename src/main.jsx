8d8873446081ef09b81f222000b33394eec55ccc
sharleezy <97629596+sharleezy@users.noreply.github.com>
2025-08-10 16:57:01 -0500

    first

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App.jsx';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import DevWorkPage from './pages/DevWorkPage.jsx';
 import HomePage from './pages/HomePage.jsx';
 import AboutPage from './pages/AboutPage.jsx';
 
 createRoot(document.getElementById('root')).render(
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} >
           <Route index element={<HomePage />} />
@@ -16,6 +16,6 @@ createRoot(document.getElementById('root')).render(
           <Route path="about" element={<AboutPage />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
 
 );
